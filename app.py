e2aba31ae3712a2fadff6875a70c1f1be65f5c03
Sonu Kumar <sonkumar0895@gmail.com>
2021-12-30 16:35:45 +0000

    config file lint

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+"""
+  Main flask app module. Define routes and app configurations
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
+    """
+    home page html site
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
